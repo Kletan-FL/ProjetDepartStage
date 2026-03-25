437e597846ad6a9494a4c20c99d236db9a1f41b1
Kletan-FL <ethan.joussein@future-legends.work>
2026-03-25 14:36:14 +0100

    Amélioration gestion erreur DB

--- connect.php
+++ connect.php
@@ -2,7 +2,15 @@
 try {
     $conn = new PDO('sqlite:' . __DIR__ . '/database/database.sqlite');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    
 } catch (PDOException $e) {
-    echo "Erreur DB : " . $e->getMessage();
+    http_response_code(500);
+        
+    echo json_encode([
+        'Erreur' => 'Erreur DB',
+        'Details' => $e->getMessage()
+    ]);
+
     die();
 }
