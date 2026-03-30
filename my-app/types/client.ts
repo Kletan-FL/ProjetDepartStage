6dd76a28d077af390b8032baa62a76e229ff06f7
Kletan-FL <ethan.joussein@future-legends.work>
2026-03-30 08:54:59 +0200

    Ajouts des types Independant et Prestation

--- my-app/types/client.ts
+++ my-app/types/client.ts
@@ -1,8 +1,8 @@
 export type Client = {
-  IDC: number;
-  NOM: string | null;
-  SIRET: string | null;
-  VILLE: string | null;
-  DOMAINE: string | null;
-  ADRESSE: string | null;
+  IDC : number;
+  NOM : string | null;
+  SIRET : string | null;
+  VILLE : string | null;
+  DOMAINE : string | null;
+  ADRESSE : string | null;
 };
---
+++ my-app/types/independant.ts
@@ -0,0 +1,8 @@
+export type Independant = {
+    IDI : number;
+    NOM : string | null;
+    PRENOM : string | null;
+    EMAIL : string | null;
+    TELEPHONE : string | null;
+    TJM_SOUHAITE : number | null;
+};
