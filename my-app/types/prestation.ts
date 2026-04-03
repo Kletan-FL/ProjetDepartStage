8da99a2e3e4b73a432863a94b585338d944d8f32
Kletan-FL <ethan.joussein@future-legends.work>
2026-04-03 11:09:01 +0200

    Fix des types TS

--- my-app/types/prestation.ts
+++ my-app/types/prestation.ts
@@ -2,7 +2,7 @@ export type Prestation = {
   IDP: number;
   IDC: number;
   IDI: number;
-  INTITULE: string | null;
+  INTITULE: string;
   DESCRIPTION: string | null;
   DATE_DEBUT: string | null;
   DATE_FIN: string | null;
